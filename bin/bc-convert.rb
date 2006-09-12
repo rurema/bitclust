f431261946e46100677cd1d2fc49b6b48b651528
Minero Aoki <aamine@loveruby.net>
2006-09-12 18:41:17 +0000

    * bin/bc-convert.rb: convert links. * bin/bc-convert.rb: convert comments.

--- bin/bc-convert.rb
+++ bin/bc-convert.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 
+$KCODE = 'EUC'
+
 def main
   ARGF.each do |line|
     case line
+    when /\A\#/
+      puts '#@' + line
     when /\A---\s/
       puts '--- ' + convert_signature(line.rstrip)
     else
@@ -24,7 +28,7 @@ def convert_signature(line)
   when /\Aself\s*(==|===|=~)\s*(\w+)/        # self == other
     "#{$1}(#{$2})"
   when /\A([\w:\.\#]+)\s*\=(\(|\s*\w+)?/   # name=
-    "#{remove_class_spec($1)}=(#{remove_paren($2.strip)})"
+    "#{remove_class_spec($1)}=(#{remove_paren($2.to_s.strip)})"
   when /\A\w+\[(.*)\]=(.*)/                  # self[key]=
     "[]=(#{$1}, #{$2.strip})"
   when /\A[\w\:]+\[(.*)\]/                   # self[key]
@@ -54,14 +58,31 @@ def remove_class_spec(str)
   str.sub(/\A[A-Z]\w*(?:::[A-Z]\w*)*[\.\#]/, '')
 end
 
-def remove_paran(str)
+def remove_paren(str)
   str.sub(/\A\(/, '').sub(/\)\z/, '')
 end
 
 def convert_link(line)
   line.gsub(/\(\(\{(.*?)\}\)\)/) { $1 }\
-      .gsub(/\(\(\|(.*?)\|\)\)/) { $1 }
-  # convert links?
+      .gsub(/\(\(\|(.*?)\|\)\)/) { $1 }\
+      .gsub(/\(\(<(.*?)>\)\)/) { convert_href($1) }
+end
+
+def convert_href(link)
+  case link
+  when /\Atrap::(.*)/              then "[[trap:#{$1}]]"
+  when /\Aruby 1\.\S+ feature/     then "((<#{link}>))"
+  when /\Aobsolete/                then "((<obsolete>))"
+  when /\A組み込み変数\/(.*)/e     then "[[m:#{$1}]]"
+  when /\A組み込み定数\/(.*)/e     then "[[m:Kernel::#{$1}]]"
+  when /\A組み込み関数\/(.*)/e     then "[[m:Kernel\##{$1}]]"
+  when /\A([\w:]+[\#\.][^|]+)\|/   then "[[m:#{$1}]]"
+  when /\A(.*?)\/manual page\z/    then "[[man:#{$1}]]"
+  when /\A([\w:]+)\/(.*)\z/n       then "[[m:#{$1}\##{$2}]]"
+  when /\A([A-Z][\w:]*)\z/         then "[[c:#{$1}]]"
+  else
+    "[[unknown:#{link}]]"
+  end
 end
 
 main
