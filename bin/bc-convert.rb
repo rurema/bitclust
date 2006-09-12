1552b827efbcbeec50ae2f43bbe3343aca99f1cd
Minero Aoki <aamine@loveruby.net>
2006-09-12 15:54:43 +0000

    * bin/bc-convert.rb: new file.

---
+++ bin/bc-convert.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+
+def main
+  ARGF.each do |line|
+    case line
+    when /\A---\s/
+      puts '--- ' + convert_signature(line.rstrip)
+    else
+      puts convert_link(line.rstrip)
+    end
+  end
+end
+
+def convert_signature(line)
+  sig = line.sub(/\A(:|---)/, '').sub(/\(\(<ruby.*?feature>\)\)/i, '').strip
+  case sig
+  when /\A([\w:\.\#]+[?!]?)\s*(?:[\(\{]|--|->|\z)/
+       # name(arg), name{}, name,
+       # name() -- obsolete
+       # name() -> return value type
+    sig
+  when /\A[\w:]+[\.\#]([+\-<>=~*^&|%\/]+)/   # Complex#+
+    sig
+  when /\Aself\s*(==|===|=~)\s*(\w+)/        # self == other
+    "#{$1}(#{$2})"
+  when /\A([\w:\.\#]+)\s*\=(\(|\s*\w+)?/   # name=
+    "#{remove_class_spec($1)}=(#{remove_paren($2.strip)})"
+  when /\A\w+\[(.*)\]=(.*)/                  # self[key]=
+    "[]=(#{$1}, #{$2.strip})"
+  when /\A[\w\:]+\[(.*)\]/                   # self[key]
+    "[](#{$1})"
+  when /\Aself\s*([+\-<>=~*^&|%\/]+)\s*(\w)/   # self + other
+    "#{$1}(#{$2})"
+  when /\A([+\-~`])\s*\w+/                   # ~ self
+    case op = $1
+    when '+', '-' then op + '@'
+    else               op
+    end
+  when /\A(?:[\w:]+[\.\#])?(\[\]=?)/         # Matrix.[](i)
+    sig
+  when /\A([+\-<>=~*^&|%]+)/                 # +(m)
+    sig
+  when /\A([A-Z]\w+\*)/                      # HKEY_*
+    sig
+  when /\Aself([+\-<>=~*^&|%\/\[\]]+)\(\w/   # self+(other)
+    sig.sub(/\Aself/, '')
+  else
+    $stderr.puts "warning: unknown method signature: #{sig.inspect}"
+    sig
+  end
+end
+
+def remove_class_spec(str)
+  str.sub(/\A[A-Z]\w*(?:::[A-Z]\w*)*[\.\#]/, '')
+end
+
+def remove_paran(str)
+  str.sub(/\A\(/, '').sub(/\)\z/, '')
+end
+
+def convert_link(line)
+  line.gsub(/\(\(\{(.*?)\}\)\)/) { $1 }\
+      .gsub(/\(\(\|(.*?)\|\)\)/) { $1 }
+  # convert links?
+end
+
+main
