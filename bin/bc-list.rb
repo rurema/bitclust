890cc2fafc93835d8747d058367cebc8df92036b
Minero Aoki <aamine@loveruby.net>
2006-09-12 14:48:10 +0000

    * bin/bc-list.rb: new file.

---
+++ bin/bc-list.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'bitclust'
+require 'pp'
+
+def main
+  db = BitClust::Database.new(nil)
+  parser = BitClust::RRDParser.new(db)
+  ARGV.each do |path|
+    pp parse_file(path)
+  end
+end
+
+def parse_file(filename)
+  libname = File.basename(filename, '.rd')
+  File.open(ARGV[0]) {|f|
+    preproc = BitClust::Preprocessor.wrap(f, {"version" => "1.9.0"})
+    return parser.parse(preproc, libname, filename)
+  }
+end
+
+main
