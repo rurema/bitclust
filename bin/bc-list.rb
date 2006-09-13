3e3a0e35db791ecd86f36854fa2eb985ced66c0f
Minero Aoki <aamine@loveruby.net>
2006-09-13 16:06:45 +0000

    * bin/bc-list.rb: parse command line options.

--- bin/bc-list.rb
+++ bin/bc-list.rb
@@ -7,12 +7,27 @@ $LOAD_PATH.unshift((bindir + '../lib').realpath)
 
 require 'bitclust'
 require 'pp'
+require 'optparse'
 
 def main
+  parser = OptionParser.new
+  parser.banner = "Usage: #{File.basename($0, '.*')} <file>..."
+  parser.on('--help', 'Prints this message and quit.') {
+    puts parser.help
+    exit
+  }
+  begin
+    parser.parse!
+  rescue OptionParser::ParseError => err
+    $stderr.puts err.message
+    exit 1
+  end
+
   db = BitClust::Database.new(nil)
   parser = BitClust::RRDParser.new(db)
   ARGV.each do |path|
-    pp parser.parse_file(path, {"version" => "1.9.0"})
+    libname = File.basename(path, '.rd')
+    pp parser.parse_file(path, libname, {"version" => "1.9.0"})
   end
 end
 
