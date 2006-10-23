e6f194180d11d247614ae0e9fffeeea2e48a1a0e
Minero Aoki <aamine@loveruby.net>
2006-10-23 10:45:37 +0000

    * bin/bc-rdoc.rb: now have subcommands instead of options. * bin/bc-rdoc.rb: --list option -> list subcommand. * bin/bc-rdoc.rb: --diff option -> diff subcommand. * bin/bc-rdoc.rb: new subcommand history.

--- bin/bc-rdoc.rb
+++ bin/bc-rdoc.rb
@@ -15,76 +15,175 @@ require 'pp'
 require 'optparse'
 
 class ApplicationError < StandardError; end
+class RiClassNotFound < ApplicationError; end
 
 def main
   Signal.trap(:PIPE) { exit 1 } rescue nil   # Win32 does not have SIGPIPE
   Signal.trap(:INT) { exit 1 }
 
-  prefix = nil
-  mode = :list
-  type = :name
   parser = OptionParser.new
-  parser.banner = "Usage: #{File.basename($0)} <classname>"
-  parser.on('-d', '--database=PREFIX', 'BitClust database path') {|path|
-    prefix = path
-  }
-  parser.on('--diff', 'Show difference between RD and RDoc') {
-    mode = :diff
-  }
-  parser.on('-c', '--content', 'Prints method description') {
-    type = :content
-  }
+  parser.banner = <<-EndUsage
+Usage: #{File.basename($0)} (list|diff|history) [options]"
+
+Subcommands:
+    list        List methods stored in ri database.
+    diff        Compare between BitClust and ri database.
+    history     Show class/method history stored in ri database.
+
+Global Options:
+EndUsage
   parser.on('--help', 'Prints this message and quit.') {
     puts parser.help
     exit 0
   }
   begin
-    parser.parse!
+    parser.order!
   rescue OptionParser::ParseError => err
     $stderr.puts err.message
     $stderr.puts parser.help
     exit 1
   end
 
-  # use only system database
-  path = RI::Paths.path(true, false, false, false)
-  reader = RI::RiReader.new(RI::RiCache.new(path))
-  case mode
-  when :list
-    ARGV.each do |name|
-      c = ri_lookup_class(reader, name)
-      case type
-      when :name
-        c.method_entries.each do |m|
-          puts m.fullname
-        end
-      when :content
-        fmt = Formatter.new
-        c.method_entries.each do |m|
-          puts fmt.method_info(reader.get_method(m))
-        end
+  subcommands = {
+    'list'    => ListCommand.new,
+    'diff'    => DiffCommand.new,
+    'history' => HistoryCommand.new
+  }
+  subcommands['hist'] = subcommands['history']
+  unless ARGV[0]
+    $stderr.puts 'no subcommand given'
+    $stderr.puts parser.help
+    exit 1
+  end
+  unless subcommands.key?(ARGV[0])
+    $stderr.puts "unknown subcommand: #{ARGV[0].inspect}"
+    $stderr.puts parser.help
+    exit 1
+  end
+  sub = subcommands[ARGV.shift]
+  begin
+    sub.parse ARGV
+  rescue OptionParser::ParseError => err
+    $stderr.puts err.message
+    $stderr.puts sub.parser.help
+    exit 1
+  end
+  sub.exec
+rescue Errno::EPIPE
+  exit 1
+rescue ApplicationError, BitClust::UserError => err
+  $stderr.puts err.message
+  exit 1
+end
+
+
+class Subcommand
+
+  def open_ri_database(prefix)
+    if prefix
+      RiDatabase.open(prefix, nil)
+    else
+      RiDatabase.open_system_db
+    end
+  end
+
+end
+
+
+class ListCommand < Subcommand
+
+  def initialize
+    @prefix = nil
+    @type = :name
+    @parser = OptionParser.new
+    @parser.banner = "Usage: #{File.basename($0, '.*')} list"
+    @parser.on('--ri-database=PREFIX', 'Ri database prefix') {|path|
+      @prefix = path
+    }
+    @parser.on('-c', '--content', 'Prints method description') {
+      @type = :content
+    }
+    @parser.on('--help', 'Prints this message and quit.') {
+      puts @parser.help
+      exit 0
+    }
+  end
+
+  attr_reader :parser
+
+  def parse(argv)
+    @parser.parse! argv
+    unless argv.size == 1
+      $stderr.puts "class name not given"
+      exit 1
+    end
+    @classname = argv[0]
+    @ri = open_ri_database(@prefix)
+  end
+
+  def exec
+    c = @ri.lookup_class(@classname)
+    case @type
+    when :name
+      c.method_entries.each do |m|
+        puts m.fullname
+      end
+    when :content
+      fmt = Formatter.new
+      c.method_entries.each do |m|
+        puts fmt.method_info(@ri.get_method(m))
       end
     end
-  when :diff
-    unless prefix
-      $stderr.puts 'missing database prefix.  Use -d option'
+  end
+
+end
+
+
+class DiffCommand < Subcommand
+
+  def initialize
+    @bcprefix = nil
+    @riprefix = nil
+    @type = :name
+    @parser = OptionParser.new
+    @parser.banner = "Usage: #{File.basename($0, '.*')} diff --bc=PATH --ri=PATH <classname>"
+    @parser.on('--bc-database=PREFIX', 'BitClust database prefix') {|path|
+      @bcprefix = path
+    }
+    @parser.on('--ri-database=PREFIX', 'Ri database prefix') {|path|
+      @riprefix = path
+    }
+    @parser.on('-c', '--content', 'Prints method description') {
+      @type = :content
+    }
+    @parser.on('--help', 'Prints this message and quit.') {
+      puts @parser.help
+      exit 0
+    }
+  end
+
+  attr_reader :parser
+
+  def parse(argv)
+    @parser.parse! argv
+    unless @bcprefix
+      $stderr.puts 'missing BitClust database prefix.  Use --bc option'
       exit 1
     end
-    unless ARGV.size == 1
-      $stderr.puts "Usage: #{$0} --diff -d DBPATH <classname>"
+    @bc = BitClust::Database.new(@bcprefix)
+    @ri = open_ri_database(@riprefix)
+    unless argv.size == 1
+      $stderr.puts "wrong number of arguments (#{argv.size} for 1)"
+      $stderr.puts @parser.help
       exit 1
     end
-    cname = ARGV[0]
-    db = BitClust::Database.new(prefix)
-    begin
-      bc_class = db.fetch_class(cname)
-    rescue BitClust::ClassNotFound
-      $stderr.puts "warning: class #{cname} not exist in BitClust database"
-      bc_class = db.get_class(cname)
-    end
-    ri_class = ri_lookup_class(reader, cname)
-    win, lose = *diff_class(bc_class, ri_class, reader)
-    case type
+    @classname = argv[0]
+  end
+
+  def exec
+    @ri.current_class = @classname
+    win, lose = *diff_class(bc_lookup_class(@classname), @ri)
+    case @type
     when :name
       win.each do |m|
         puts "+ #{m.fullname}"
@@ -99,57 +198,193 @@ def main
         puts fmt.method_info(m.entry)
       end
     end
-  else
-    raise "must not happen: #{mode.inspect}"
   end
-rescue Errno::EPIPE
-  exit 1
-rescue ApplicationError, BitClust::UserError => err
-  $stderr.puts err.message
-  exit 1
-end
 
-def ri_lookup_class(reader, name)
-  ns = reader.top_level_namespace.first
-  name.split('::').each do |const|
-    ns = ns.contained_class_named(const) or
-        raise ApplicationError, "no such class in RDoc database: #{name}"
+  def bc_lookup_class(classname)
+    @bc.fetch_class(classname)
+  rescue BitClust::ClassNotFound
+    $stderr.puts "warning: class #{classname} not exist in BitClust database"
+    @bc.get_class(classname)
   end
-  ns
-end
 
-def diff_class(bc, ri, reader)
-  unzip(diff_entries(bc, bc.singleton_methods, reader.singleton_methods(ri)),
-        diff_entries(bc, bc.instance_methods,  reader.instance_methods(ri)))\
-      .map {|list| list.flatten }
-end
-
-def unzip(*tuples)
-  [tuples.map {|s, i| s }, tuples.map {|s, i| i }]
-end
-
-def diff_entries(bc_class, _bc, _ri)
-  bc = _bc.map {|m| m.names.map {|name| BCMethodEntry.new(name, m) } }.flatten.uniq.sort
-  ri = _ri.map {|m|
-         [RiMethodEntry.new(m.name, m)] +
-             m.aliases.map {|a| RiMethodEntry.new(a.name, m) }
-       }.flatten.uniq.sort
-  win = bc - ri
-  lose0 = ri - bc
-  lose = lose0.reject {|m| true_exist?(bc_class, m) }
-  [win, lose]
-end
-
-def true_exist?(klass, m)
-  if m.singleton_method?
-    klass.singleton_method?(m.name, true)
-  else
-    klass.instance_method?(m.name, true)
+  def diff_class(bc, ri)
+    unzip(diff_entries(bc, bc_wrap(bc.singleton_methods), ri.singleton_methods),
+          diff_entries(bc, bc_wrap(bc.instance_methods),  ri.instance_methods))\
+        .map {|list| list.flatten }
   end
+
+  def bc_wrap(ents)
+    ents.map {|m|
+      m.names.map {|name| BCMethodEntry.new(name, m) }
+    }.flatten.uniq
+  end
+
+  def unzip(*tuples)
+    [tuples.map {|s, i| s }, tuples.map {|s, i| i }]
+  end
+
+  def diff_entries(bc_class, bc, ri)
+    bc = bc.sort
+    ri = ri.sort
+    [bc - ri, (ri - bc).reject {|m| true_exist?(bc_class, m) }]
+  end
+
+  def true_exist?(c, m)
+    if m.singleton_method?
+      c.singleton_method?(m.name, true)
+    else
+      c.instance_method?(m.name, true)
+    end
+  end
+
 end
 
-def uncapsule(list)
-  list.map {|m| m.entry }.uniq.sort_by {|ent| ent.name }
+
+class HistoryCommand < Subcommand
+
+  def initialize
+    @riprefix = nil
+    @parser = OptionParser.new
+    @parser.banner = "Usage: #{File.basename($0, '.*')} history --ri=PATH <classname>"
+    @parser.on('--ri-database=PREFIX', 'Ri database prefix') {|path|
+      @riprefix = path
+    }
+    @parser.on('--help', 'Prints this message and quit.') {
+      puts @parser.help
+      exit 0
+    }
+  end
+
+  attr_reader :parser
+
+  def parse(argv)
+    @parser.parse! argv
+    unless @riprefix
+      $stderr.puts 'ri database not given; use --ri option'
+      exit 1
+    end
+    @ris = Dir.glob("#{@riprefix}/1.*").map {|dir|
+      RiDatabase.open(dir, File.basename(dir))
+    }
+    if @ris.empty?
+      $stderr.puts 'wrong ri database directory; directories like <path>/1.8.3/, <path>/1.8.4/, ... must exist'
+      exit 1
+    end
+    unless argv.size == 1
+      $stderr.puts "wrong number of arguments (#{argv.size} for 1)"
+      $stderr.puts @parser.help
+      exit 1
+    end
+    @classname = argv[0]
+  end
+
+  def exec
+    @ris.each do |ri|
+      ri.current_class = @classname
+    end
+    s = {}
+    i = {}
+    @ris.each do |ri|
+      ri.singleton_methods.each do |m|
+        (s[m] ||= []).push ri.version
+      end
+      ri.instance_methods.each do |m|
+        (i[m] ||= []).push ri.version
+      end
+    end
+    namecols = calculate_n_namecols(s.keys + i.keys)
+    versions = @ris.map {|ri| ri.version }
+    print_header namecols, versions
+    print_records namecols, versions, (s.to_a + i.to_a)
+  end
+
+  def calculate_n_namecols(ms)
+    tabstop = 8
+    maxnamelen = ms.map {|m| m.fullname.size }.max
+    (maxnamelen / tabstop + 1) * tabstop
+  end
+
+  def print_header(namecols, versions)
+    print ' ' * namecols
+    versions.each do |ver|
+      printf '%4s', ver.tr('.', '')
+    end
+    puts
+  end
+
+  def print_records(namecols, versions, records)
+    veridx = {}
+    versions.each_with_index do |ver, idx|
+      veridx[ver] = idx
+    end
+    records.sort_by {|m, vers| m.fullname }.each do |m, vers|
+      printf "%-#{namecols}s", m.fullname
+
+      fmt = '%4s' * versions.size
+      cols = ['-'] * versions.size
+      vers.each do |ver|
+        cols[veridx[ver]] = 'o'
+      end
+      printf fmt, *cols
+      puts
+    end
+  end
+
+end
+
+
+class RiDatabase
+  def RiDatabase.open_system_db
+    new(RI::Paths.path(true, false, false, false), RUBY_VERSION)
+  end
+
+  def RiDatabase.open(dir, version)
+    new(RI::Paths.path(false, false, false, false, dir), version)
+  end
+
+  def initialize(ripath, version)
+    @ripath = ripath
+    @reader = RI::RiReader.new(RI::RiCache.new(@ripath))
+    @version = version
+  end
+
+  attr_reader :version
+
+  def get_method(m)
+    @reader.get_method(m)
+  end
+
+  def current_class=(name)
+    @klass = lookup_class(name)
+    @singleton_methods = wrap_entries(@reader.singleton_methods(@klass))
+    @instance_methods = wrap_entries(@reader.instance_methods(@klass))
+  rescue RiClassNotFound
+    @klass = nil
+    @singleton_methods = []
+    @instance_methods = []
+  end
+
+  attr_reader :class
+  attr_reader :singleton_methods
+  attr_reader :instance_methods
+
+  def lookup_class(name)
+    ns = @reader.top_level_namespace.first
+    name.split('::').each do |const|
+      ns = ns.contained_class_named(const) or
+          raise RiClassNotFound, "no such class in RDoc database: #{name}"
+    end
+    ns
+  end
+
+  private
+
+  def wrap_entries(ents)
+    ents.map {|m|
+      [RiMethodEntry.new(m.name, m)] +
+      m.aliases.map {|a| RiMethodEntry.new(a.name, m) }
+    }.flatten.uniq
+  end
 end
 
 class Ent
