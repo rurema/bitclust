3feb283ad756e5d870c18d2c99d6005a513d91c8
Takashi Tamura <null@jp.rubyist.net>
2007-12-23 12:48:00 +0000

    * test/test_preprocessor.rb: add a new test.

---
+++ test/test_preprocessor.rb
@@ -0,0 +1,41 @@
+require 'test/unit'
+require 'bitclust/preprocessor'
+require 'stringio'
+
+class TestPreprocessor < Test::Unit::TestCase
+  include BitClust
+  def test_eval_cond
+    params = { 'version' => '1.8.7' }
+
+    [
+     ['#@if( version > "1.8.0")', true],
+     ['#@if( version == "1.8.7")', true],
+     ['#@if( version != "1.9.0")', true],
+     ['#@since 1.8.0', true],
+     ['#@since 1.8.7', true],
+     ['#@until 1.8.7', false],
+     ['#@until 1.9.0', true],
+     ['#@if( version > "1.8.0" and version < "1.9.0")', true],
+     ['#@if( version > "1.8.9" and version < "1.9.0")', false],
+     ['#@if( version > "1.8.9" or version < "1.9.0")', true],
+     ['#@if( version < "1.8.0" or version > "1.9.0")', false],
+     ['#@if( version > "1.8.0" and version < "1.9.0" and version < "1.9.1")', true],
+     ['#@if( version > "1.8.0" and version > "1.9.0" or version != "1.9.1")', true],
+    ].each{|cond, expected_result|
+      s = <<HERE
+#{cond}
+a
+\#@else
+b
+\#@end
+HERE
+      ret = Preprocessor.wrap(StringIO.new(s), params).to_a
+      if expected_result
+        assert_equal(["a\n"], ret)
+      else
+        assert_equal(["b\n"], ret)
+      end
+    }
+  end
+end
+
