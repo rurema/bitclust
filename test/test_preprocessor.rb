14ae4924e772dc4e5a1ec9c17ac92c3b903f9507
Takashi Tamura <null@jp.rubyist.net>
2007-12-24 04:46:17 +0000

    * test/test_preprocessor.rb (TestPreprocessor::test_eval_cond):   add new assertions.

--- test/test_preprocessor.rb
+++ test/test_preprocessor.rb
@@ -8,19 +8,26 @@ class TestPreprocessor < Test::Unit::TestCase
     params = { 'version' => '1.8.7' }
 
     [
-     ['#@if( version > "1.8.0")', true],
-     ['#@if( version == "1.8.7")', true],
-     ['#@if( version != "1.9.0")', true],
-     ['#@since 1.8.0', true],
-     ['#@since 1.8.7', true],
+     ['#@if( version > "1.8.0")',  true ],
+     ['#@if( version < "1.8.0")',  false],
+     ['#@if( version <= "1.8.7")', true ],
+     ['#@if( version >= "1.9.1")', false],
+     ['#@if( version == "1.8.7")', true ],
+     ['#@if( version != "1.9.0")', true ],
+     ['#@if( "1.9.0" != version)', true ],
+     
+     ['#@since 1.8.0', true ],
+     ['#@since 1.8.7', true ],
      ['#@until 1.8.7', false],
-     ['#@until 1.9.0', true],
-     ['#@if( version > "1.8.0" and version < "1.9.0")', true],
+     ['#@until 1.9.0', true ],
+     
+     ['#@if( version > "1.8.0" and version < "1.9.0")', true ],
      ['#@if( version > "1.8.9" and version < "1.9.0")', false],
-     ['#@if( version > "1.8.9" or version < "1.9.0")', true],
-     ['#@if( version < "1.8.0" or version > "1.9.0")', false],
-     ['#@if( version > "1.8.0" and version < "1.9.0" and version < "1.9.1")', true],
-     ['#@if( version > "1.8.0" and version > "1.9.0" or version != "1.9.1")', true],
+     ['#@if( version > "1.8.9" or version < "1.9.0")',  true ],
+     ['#@if( version < "1.8.0" or version > "1.9.0")',  false],
+     ['#@if( version > "1.8.0" and version < "1.9.0" and version < "1.9.1")', true ],
+     ['#@if( version > "1.8.0" and version < "1.9.0" and version > "1.9.1")', false],
+     ['#@if( version < "1.8.0" and version > "1.9.0" or "1.9.1" != version)', true ],
     ].each{|cond, expected_result|
       s = <<HERE
 #{cond}
