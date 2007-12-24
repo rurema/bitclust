0cb141c249f82afa915b25b8d3fdf2da18a07ac2
Takashi Tamura <null@jp.rubyist.net>
2007-12-24 14:42:44 +0000

    add new tests

--- test/test_rdcompiler.rb
+++ test/test_rdcompiler.rb
@@ -99,5 +99,83 @@ foo
 HERE
   end
 
-  
+  def test_method
+    
+    ret = @c.compile <<'HERE'
+--- hoge
+foo
+bar
+ text
+HERE
+    assert_equal <<'HERE', ret
+<dt><code>hoge</code></dt>
+<dd>
+<p>
+foo
+bar
+</p>
+<pre>
+text
+</pre>
+</dd>
+HERE
+    
+    ret = @c.compile <<'HERE'
+--- <=>
+HERE
+   assert_equal <<'HERE', ret
+<dt><code>&lt;=&gt;</code></dt>
+<dd>
+</dd>
+HERE
+    
+    c = BitClust::RDCompiler.new(@u, 1, {:force => true})
+    ret = c.compile <<'HERE'
+--- hoge1
+--- hoge2
+bar
+HERE
+    
+    assert_equal <<'HERE', ret
+<dl>
+<dt><code>hoge1</code></dt>
+<dt><code>hoge2</code></dt>
+<dd>
+<p>
+bar
+</p>
+</dd>
+</dl>
+HERE
+
+  end
+
+  def test_ul
+        
+    ret = @c.compile <<'HERE'
+ * hoge1
+ * hoge2
+HERE
+   assert_equal <<'HERE', ret
+<ul>
+<li>hoge1</li>
+<li>hoge2</li>
+</ul>
+HERE
+        
+    ret = @c.compile <<'HERE'
+ * hoge1
+
+ * hoge2
+HERE
+   assert_equal <<'HERE', ret
+<ul>
+<li>hoge1</li>
+</ul>
+<ul>
+<li>hoge2</li>
+</ul>
+HERE
+ 
+  end
 end
