87746f1b6f70faf26f495742cd33eff0b70ca2ed
okkez <okkez000@gmail.com>
2009-08-08 05:33:29 +0000

    remove trailing white spaces and add meessages.

--- test/test_simplesearcher.rb
+++ test/test_simplesearcher.rb
@@ -3,7 +3,7 @@ require 'bitclust'
 require 'bitclust/simplesearcher'
 require 'optparse'
 
- 
+
 class TestSearcher <  Test::Unit::TestCase
 
   def setup
@@ -36,13 +36,13 @@ HERE
      ['Hoge::B','Hoge::Bar'],
      ['B b',    'bar'],
      [' B b c ','bar'],
-     ['b B',    'bar'], 
-    ].each{|q, expected|      
+     ['b B',    'bar'],
+    ].each{|q, expected|
       ret = search_pattern(@db, q)
-      assert_not_equal([], ret )
-      assert_equal(expected, ret[0].name) 
+      assert_not_equal([], ret, q)
+      assert_equal(expected, ret[0].name, q)
     }
-    assert_equal([], search_pattern(@db, " "))
-    assert_equal([], search_pattern(@db, ""))
+    assert_equal([], search_pattern(@db, " "), 'space')
+    assert_equal([], search_pattern(@db, ""), 'blank')
   end
 end
