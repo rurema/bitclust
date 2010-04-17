139465a803569a8115591f1adeb3ab647bbb521c
wanabe <s.wanabe@gmail.com>
2010-04-17 00:59:51 +0000

    * test/db-test/*: removed.
    * test/test_bitclust.rb: create db to temporary dir.

--- test/test_bitclust.rb
+++ test/test_bitclust.rb
@@ -1,14 +1,39 @@
 require 'bitclust'
 require 'stringio'
 require 'test/unit'
+require 'tmpdir'
+require 'fileutils'
 
 BITCLUST_DIR = File.dirname(File.dirname(File.expand_path(__FILE__)))
 require "#{BITCLUST_DIR}/bin/bitclust.rb"
 
 class TestBitClust < Test::Unit::TestCase
   def setup
-    @db = "-d#{BITCLUST_DIR}/test/db-test"
+    @prefix = 'db'
+
+    @pwd = Dir.pwd
+    @tmpdir = Dir.mktmpdir
+    src = "#{@tmpdir}/function/public_func"
+    @srcdir = Dir.mkdir File.dirname(src)
+    File.open(src, 'w') do |file|
+      file.puts <<'HERE'
+filename=test.c
+macro=false
+private=false
+type=VALUE
+name=public_func
+params=()
+
+
+This is public function.
+HERE
+    end
+    
+    @db = "-d#{@tmpdir}"
     @out = StringIO.new
+  end
+  def teardown
+    FileUtils.rm_r(@tmpdir, :force => true)
   end
   def capi(*argv)
     if argv.first.is_a? Symbol
