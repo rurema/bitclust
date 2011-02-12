ec92fee6110e115e6c808831d880ce0345202e1b
okkez <okkez000@gmail.com>
2011-02-12 13:48:20 +0000

    * test/run_test.rb: add.

---
+++ test/run_test.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'pathname'
+
+gem 'test-unit'
+require 'test/unit'
+require 'test/unit/notify'
+
+base_dir = Pathname.new(__FILE__).dirname.expand_path
+top_dir = (base_dir + '..').expand_path
+lib_dir = top_dir + 'lib'
+
+$LOAD_PATH.unshift(lib_dir.to_s)
+
+exit Test::Unit::AutoRunner.run(true, base_dir)
