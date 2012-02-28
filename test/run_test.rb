2a7290f459e8f5a3efc6428fcd5cc4a7d3cb6bb5
okkez <okkez000@gmail.com>
2012-02-28 11:54:33 +0000

    use test-unit-rr.

--- test/run_test.rb
+++ test/run_test.rb
@@ -4,6 +4,7 @@ require 'pathname'
 gem 'test-unit'
 require 'test/unit'
 require 'test/unit/notify'
+require 'test/unit/rr'
 
 base_dir = Pathname.new(__FILE__).dirname.expand_path
 top_dir = (base_dir + '..').expand_path
