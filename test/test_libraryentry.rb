bc7009a32f193c16011f3795ace7fe7ede1461dc
Ippei Obayashi <ohai@kmc.gr.jp>
2012-08-10 14:33:12 +0000

    Add a test for LibraryEntry#error_classes

--- test/test_libraryentry.rb
+++ test/test_libraryentry.rb
@@ -17,6 +17,8 @@ require hoge/bar
 = class Bar < Hoge
 == Class Methods
 --- bar
+= class Err < Exception
+= class Err2 < Err
 DOC
     @lib_entry, @db = BitClust::RRDParser.parse(s, 'hoge')
     @sublibrary = LibraryEntry.new(@db, 'testsub')
@@ -27,5 +29,9 @@ DOC
     assert_equal(['testsub'], @lib_entry.sublibraries.map(&:name))
     @lib_entry.sublibrary(@sublibrary)
     assert_equal(['testsub'], @lib_entry.sublibraries.map(&:name))
+  end
+
+  def test_error_classes
+    assert_equal(["Err", "Err2"], @lib_entry.error_classes.map(&:name).sort)
   end
 end
