148ae675a093682a3c6a3cf1a6fb6dbfcbb573a4
ridec <squ.web@gmail.com>
2013-09-05 13:35:17 +0900

    Use mkdir_p instead of mkdir

--- lib/bitclust/generators/epub.rb
+++ lib/bitclust/generators/epub.rb
@@ -13,7 +13,7 @@ module BitClust
 
       def copy_static_files(templatedir, outputdir)
         FileUtils.copy(templatedir + "mimetype", outputdir)
-        FileUtils.mkdir(outputdir + "META-INF")
+        FileUtils.mkdir_p(outputdir + "META-INF")
         FileUtils.copy(templatedir + "container.xml", outputdir + "META-INF")
       end
     end
