61ad731248438e474f709e1ef8e7115386ead086
ridec <squ.web@gmail.com>
2013-09-05 11:31:40 +0900

    Add EPUB generator skelton

---
+++ lib/bitclust/generators/epub.rb
@@ -0,0 +1,13 @@
+module BitClust
+  module Generators
+    class EPUB
+      def initialize(options = {})
+        # TODO implement
+      end
+
+      def generate
+        # TODO implement
+      end
+    end
+  end
+end
