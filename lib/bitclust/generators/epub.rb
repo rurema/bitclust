e9a8338f1cb88c61b8477ce9fec812c44d9ea3cb
ridec <squ.web@gmail.com>
2013-09-05 12:06:35 +0900

    Implement generation of static files

--- lib/bitclust/generators/epub.rb
+++ lib/bitclust/generators/epub.rb
@@ -1,12 +1,20 @@
+require 'fileutils'
+
 module BitClust
   module Generators
     class EPUB
       def initialize(options = {})
-        # TODO implement
+        @options = options.dup
       end
 
       def generate
-        # TODO implement
+        copy_static_files(@options[:templatedir], @options[:outputdir])
+      end
+
+      def copy_static_files(templatedir, outputdir)
+        FileUtils.copy(templatedir + "/mimetype", outputdir)
+        FileUtils.mkdir(outputdir + "/META-INF")
+        FileUtils.copy(templatedir + "/container.xml", outputdir + "/META-INF")
       end
     end
   end
