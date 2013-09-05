f1871e349d6535a9d37ad0231c32b7557b79938f
ridec <squ.web@gmail.com>
2013-09-05 15:23:40 +0900

    Implement generation of CSS and favicon

--- lib/bitclust/generators/epub.rb
+++ lib/bitclust/generators/epub.rb
@@ -8,9 +8,14 @@ module BitClust
         @options = options.dup
       end
 
+      CONTENTS_DIR_NAME = 'OEBPS'
+
       def generate
         epub_directory = Dir.mktmpdir("epub-temp-", @options[:outputdir])
-        copy_static_files(@options[:templatedir], Pathname.new(epub_directory))
+        epub_directory = Pathname.new(epub_directory)
+        contents_directory = epub_directory + CONTENTS_DIR_NAME
+        copy_static_files(@options[:templatedir], epub_directory)
+        copy_assets(@options[:themedir], contents_directory)
         FileUtils.remove_entry_secure(epub_directory) unless @options[:keep]
       end
 
@@ -18,6 +23,15 @@ module BitClust
         FileUtils.copy(templatedir + "mimetype", epub_directory)
         FileUtils.mkdir_p(epub_directory + "META-INF")
         FileUtils.copy(templatedir + "container.xml", epub_directory + "META-INF")
+      end
+
+      CSS_NAME = 'style.css'
+      FAVICON_NAME = 'rurema.png'
+
+      def copy_assets(themedir, contents_directory)
+        FileUtils.mkdir_p(contents_directory)
+        FileUtils.copy(themedir + CSS_NAME, contents_directory)
+        FileUtils.copy(themedir + FAVICON_NAME, contents_directory)
       end
     end
   end
