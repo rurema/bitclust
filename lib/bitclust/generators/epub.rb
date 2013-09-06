547629dc969fc8a4ea3b6600fff87c50d64cecde
ridec <squ.web@gmail.com>
2013-09-06 15:05:25 +0900

    Implement generation of contents and epub

--- lib/bitclust/generators/epub.rb
+++ lib/bitclust/generators/epub.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 require 'tmpdir'
+require 'erb'
 
 require 'bitclust/subcommands/statichtml_command'
 
@@ -16,9 +17,13 @@ module BitClust
         make_tmp_dir("epub-temp-", @options[:outputdir], @options[:keep]) do |epub_directory|
           contents_directory = epub_directory + CONTENTS_DIR_NAME
           copy_static_files(@options[:templatedir], epub_directory)
+
           html_options = @options.dup
           html_options[:outputdir] = contents_directory
           generate_html(html_options)
+
+          generate_contents_file(@options[:templatedir], epub_directory, @options[:fs_casesensitive])
+          pack_epub(@options[:outputdir] + @options[:filename], epub_directory)
         end
       end
 
@@ -49,6 +54,27 @@ module BitClust
         cmd.exec(argv, options)
       end
 
+      def generate_contents_file(template_directory, epub_directory, fs_casesensitive)
+        items = []
+        glob_relative_path(epub_directory, "#{CONTENTS_DIR_NAME}/class/*.html").each do |path|
+          items << {
+            :id => decodename_package(path.basename(".*").to_s, fs_casesensitive),
+            :path => path
+          }
+        end
+        contents = ERB.new(File.read(template_directory + "contents"), nil, "-").result(binding)
+        open(epub_directory + "contents.opf", "w") do |f|
+          f.write contents
+        end
+      end
+
+      def pack_epub(output_path, epub_directory)
+        Dir.chdir(epub_directory.to_s) do
+          system("zip -0 -X mimetype")
+          system("zip -x  ./*")
+        end
+      end
+
       def glob_relative_path(path, pattern)
         relative_paths = []
         absolute_path_to_search = Pathname.new(path).realpath
@@ -57,6 +83,14 @@ module BitClust
           relative_paths << absolute_path.relative_path_from(absolute_path_to_search)
         end
         relative_paths
+      end
+
+      def decodename_package(str, fs_casesensitive)
+        if fs_casesensitive
+          NameUtils.decodename_url(str)
+        else
+          NameUtils.decodename_fs(str)
+        end
       end
     end
   end
