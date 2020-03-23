81e770b1ff8492381826381395e3870731b5a6b0
scivola <>
2020-03-23 22:14:52 +0900

    コピー時，alert をやめ，「COPY」を「COPIED」とする

--- theme/default/script.js
+++ theme/default/script.js
@@ -2,31 +2,32 @@
   window.onload = function() {
     const elems = document.getElementsByClassName('highlight')
 
-    let backet = document.createElement('div')
+    let tempDiv = document.createElement('div')
 
     Array.prototype.forEach.call(elems,
       function(elem) {
         // sample code without caption
-        backet.innerHTML = elem.innerHTML
-        const caption = backet.getElementsByClassName("caption")[0]
-        if (caption) backet.removeChild(caption)
+        tempDiv.innerHTML = elem.innerHTML
+        const caption = tempDiv.getElementsByClassName("caption")[0]
+        if (caption) tempDiv.removeChild(caption)
 
         // textarea for preserving the copy text
         const copyText = document.createElement('textarea')
         copyText.setAttribute('class', 'highlight__copy-text')
-        copyText.innerHTML = backet.textContent.replace(/^\n+/, "").replace(/\n{2,}$/, "\n")
+        copyText.innerHTML = tempDiv.textContent.replace(/^\n+/, "").replace(/\n{2,}$/, "\n")
         elem.appendChild(copyText)
 
         // COPY button
         const btn = document.createElement('div')
         btn.setAttribute('class', 'highlight__copy-button')
-        btn.textContent = "COPY"
+        // btn.textContent = "COPY"
         elem.insertBefore(btn, elem.firstChild)
 
         btn.onclick = function(){
           copyText.select()
           document.execCommand("copy")
-          alert("Copied to your Clipboard.")
+          btn.classList.add("copied")
+          window.setTimeout(function() { btn.classList.remove("copied") }, 1000)
         }
       }
     )
