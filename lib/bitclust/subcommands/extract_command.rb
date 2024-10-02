e8b6b706d8e46cd9d460195efef2d7058002319d
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2024-10-02 13:40:22 +0900

    Add workaround for `rescue SomeModule`

--- lib/bitclust/subcommands/extract_command.rb
+++ lib/bitclust/subcommands/extract_command.rb
@@ -28,6 +28,8 @@ module BitClust
               show_library lib
             end
           rescue WriterError => err
+            # @type var err: StandardError
+            err = _ = err
             raise if $DEBUG
             $stderr.puts "#{File.basename($0, '.*')}: FAIL: #{err.message}"
             success = false
