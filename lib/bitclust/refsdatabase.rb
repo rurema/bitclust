7d90ee6123531d7e06a1fd2ca74db7184109a880
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2024-12-05 15:48:08 +0900

    Update refsdatabase.rbs

--- lib/bitclust/refsdatabase.rb
+++ lib/bitclust/refsdatabase.rb
@@ -12,15 +12,18 @@ module BitClust
   class RefsDatabase
     def self.load(src)
       if src.respond_to?(:to_str)
+        # @type var src: _ToStr
         buf = fopen(src.to_str, 'r:UTF-8'){|f| f.read}
       elsif src.respond_to?(:to_io)
+        # @type var src: _ToIO
         buf = src.to_io.read
       else
+        # @type var src: _Reader
         buf = src.read
       end
 
       refs = self.new
-      buf.each_line{|l|
+      buf&.each_line{|l|
         if /((?:\\,|[^,])+),((?:\\,|[^,])+),((?:\\,|[^,])+),((?:\\,|[^,])+)\n/ =~ l
           type, id, linkid, desc = [$1, $2, $3, $4].map{|e| e&.gsub(/\\(.)/){|s| $1 == ',' ? ',' : s } }
           refs[type, id, linkid] = desc
@@ -43,13 +46,16 @@ module BitClust
 
     def save(s)
       if s.respond_to?(:to_str)
+        # @type var s: _ToStr
         path = s.to_str
         io = fopen(path, 'w:UTF-8')
       elsif s.respond_to?(:to_io)
+        # @type var s: _ToIO
         io = s.to_io
       else
         io = s
       end
+      # @type var io: IO
 
       @h.sort.each{|k, v|
         io.write(  [k, v].flatten.map{|e| e.gsub(/,/, '\\,') }.join(',') + "\n" )
